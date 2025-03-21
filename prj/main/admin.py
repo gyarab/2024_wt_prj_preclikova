a927604440825ab8915d1924eac30c16e0d2115c
Eliška Preclíková <eliska.preclikova.s@gyarab.cz>
2025-03-21 10:30:52 +0100

    oprava

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from.models import Book
+from.models import Book, Author
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "author", "description"]
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name","birth_year","description"]
 
 # Register your models here.
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -3,4 +3,18 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=300)
     year = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, default="")
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return f"Book <{self.id}> {self.name}"
+        
+class Author(models.Model):
+   name = models.CharField(max_length=300)
+   birth_year = models.IntegerField(blank=True, null=True)
+   description = models.TextField(blank=True, default="")
+
+   def __str__(self):
+        return f"Author <{self.id}> {self.name}"
+    
+   
