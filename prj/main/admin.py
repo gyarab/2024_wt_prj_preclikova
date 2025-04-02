b68be7e21eb29e7ff5eee0de62ab6a0090d33b05
Eliska Preclikova <eliska.preclikova.s@gyarab.cz>
2025-04-02 21:53:00 +0200

    Vývoj modelu

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from.models import Book, Author
+from.models import Book, Author, Genre, Reader, ReadingRecord
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "author", "description"]
+    list_display = ["id", "name", "author", "description", "genre"]
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ["id", "name","birth_year","description"]
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class ReaderAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class ReadingRecordAdmin(admin.ModelAdmin):
+    list_display = ["id", "date_add", "state",
+                    "date_start", 
+                    "date_end", "text", "rating" ]
 
 # Register your models here.
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Reader, ReaderAdmin)
+admin.site.register(ReadingRecord, ReadingRecordAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -5,6 +5,7 @@ class Book(models.Model):
     year = models.IntegerField(blank=True, null=True)
     description = models.TextField(blank=True, default="")
     author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    genre = models.ForeignKey("Genre", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Book <{self.id}> {self.name}"
@@ -16,5 +17,33 @@ class Author(models.Model):
 
    def __str__(self):
         return f"Author <{self.id}> {self.name}"
+   
+class Genre(models.Model):
+    name = models.CharField(max_length=300)
+    def __str__(self):
+        return f"Genre <{self.id}> {self.name}"
     
+class Reader(models.Model):
+   name = models.CharField(max_length=300)
+   
+   def __str__(self):
+        return f"Reader <{self.id}> {self.name}"
+   
+class ReadingRecord(models.Model):
+    reader = models.ForeignKey("Reader", on_delete=models.CASCADE, default=1, null=False)
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, default=1, null=False)
+    date_add = models.DateField()
+    state = models.TextField(max_length=1, choices={
+        "A": "Chci číst",
+        "B": "Právě čtu",  
+        "C": "Přečteno",   
+        "D": "Nechci číst"
+    })
+    date_start = models.DateField(blank=True, null=True)
+    date_end = models.DateField(blank=True, null=True)
+    text = models.TextField(blank=True, default="")
+    rating = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"ReadingRecord <{self.id}>"
    
