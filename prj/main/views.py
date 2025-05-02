7f24f09af1edc531b5ba5835d0fbd4fa696fbf38
Eliška Preclíková <eliska.preclikova.s@gyarab.cz>
2025-05-02 10:19:36 +0200

    hodina 2.5.

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,14 +1,27 @@
+
 from django.shortcuts import render
 from main.models import Book
 
 # Create your views here.
 
 def get_homepage(request): 
-    context ={ 
-        "svatek":"Libor",
+    books = Book.objects.all().order_by("title")
+
+ #   context = "svatek":"Libor",
 
         #SELECT * from Books;
-        "books": Book.objects.all()
-    }
-    return render(request, "main/homepage.html", context)
+ #       "books": Book.objects.all()
+   # }
+
+ #   print("HOST", request.get_host())
+ #   print("URL", request.path)
+
+    # check if search parameter is in the request
+    if request.GET.get("search"):
+        # filter books based on the search parameter
+        print("SEARCH", request.GET.get("search"))
+
+    return render(
+        request, "main/homepage.html"
+        )
    
